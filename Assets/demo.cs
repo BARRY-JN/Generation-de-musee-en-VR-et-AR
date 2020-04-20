9abbd95bdc3e5825de7ebcaf533f27b6e4ca7455
jn42barry <jean-noel.barry@etu.univ-amu.fr>
2020-04-20 17:14:08 +0200

    first

---
+++ Assets/demo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vInit = Vuforia.VuforiaARController.Instance;
+        vInit.RegisterVuforiaInitializedCallback(OnVuforiaStarted);
+
+    }
+
+    public void OnVuforiaStarted()
+    {
+        Vuforia.CameraDevice.Instance.SetFocusMode(Vuforia.CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+}
